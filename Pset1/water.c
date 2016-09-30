e72c30c2a4524b6385784f4c741d8d912ec710a5
Jonnokc <Jonnokc@users.noreply.github.com>
2016-09-30 16:49:46 -0500

    Update water.c

--- Pset1/water.c
+++ Pset1/water.c
@@ -1,1 +1,19 @@
 
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    int shower_length = 0;
+    int bottles_per_min = 12;
+
+    printf("Enter the length of your shower: ")
+    shower_length = GetInt();
+    total_bottles = shower_length * bottles_per_min
+
+    printf("minutes: %s\n", shower_length);
+    printf("bottles: %s\n", total_bottles);
+
+
+}
